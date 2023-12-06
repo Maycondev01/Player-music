17239a1b67ab958ca49e7b3495462deb4de9df5c
maycondev01 <mayconlundgren186@gmail.com>
2023-12-06 17:07:00 -0300

    músicas

---
+++ songs.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        src: "./src/Adele - Skyfall.mp3",
+        name: "Adele - Skyfall",
+    },
+    {
+        src: "./src/Amy Winehouse - Back To Black.mp3",
+        name: "Amy Winehouse - Back to Black",
+    },
+    {
+        src: "./src/Pearl Jam - Black.mp3",
+        name: "Pearl Jam - Black",
+    },
+];
+
